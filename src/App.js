7a6532635b7a8970394bb5c4d631cbcdb19fe39f
abdullah <78781375+Rancho-das@users.noreply.github.com>
2021-04-13 05:35:08 -0700

    add h1

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
     <div className="App">
       <h1>Abdullah hellow</h1>
       <h1>i am numan your contributor sir abdullah </h1>
+      <p>lorem</p>
+     <h1>auto deploy</h1>
     </div>
   );
 }
